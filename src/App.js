50b6fd2a380255e8c431eae364dbdc33a213798d
daniel <hdanielmm@gmail.com>
2019-10-22 19:33:32 +0200

    Add counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onKeyUpCapture={this.countCharacters.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+
+  countCharacters(event){
+    this.setState({
+      count: event.target.value.length
+    })
   }
 }
 
